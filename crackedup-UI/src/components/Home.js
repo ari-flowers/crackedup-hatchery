c4978ba44e398850617cfad86bc2fe97ce1a4bee
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2024-05-30 16:42:39 -0700

    Bootstraped Home

--- crackedup-UI/src/components/Home.js
+++ crackedup-UI/src/components/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to Cracked Up Hatchery!</h1>
-      <p>Currently under construction, check back soon :)</p>
-      <button onClick={() => alert('Login functionality coming soon!')}>Login</button>
-    </div>
+    <Container className="mt-5">
+      <div className="p-5 mb-4 bg-light rounded-3">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">Welcome to Cracked Up Hatchery!</h1>
+          <p className="fs-4">
+            Currently under construction, check back soon :)
+          </p>
+          <Button variant="primary" onClick={() => alert('Login functionality coming soon!')}>Login</Button>
+        </div>
+      </div>
+    </Container>
   );
 }
 
