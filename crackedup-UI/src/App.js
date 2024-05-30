cf77ebd3fc9bba7881b2938917d3dad4e60909e9
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2024-05-30 04:10:09 -0700

    Dashboard.js initial setup

--- crackedup-UI/src/App.js
+++ crackedup-UI/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Header from './components/Header';
 import Home from './components/Home';
-// import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard';
 // import Hatchery from './components/Hatchery';
 import Footer from './components/Footer';
 import './App.css';
@@ -14,8 +14,8 @@ function App() {
         {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/hatchery" element={<Hatchery />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+           {/* <Route path="/hatchery" element={<Hatchery />} /> */}
         </Routes>
         <Footer />
       </div>
---
+++ crackedup-UI/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Manage your dragons here.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
