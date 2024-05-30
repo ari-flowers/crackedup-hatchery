52c527da30e9b5449d840c2c8a80dd0c0e00ae25
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2024-05-30 03:49:51 -0700

    Initial commit

---
+++ crackedup-UI/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my hatchery!</h1>
+      <p>Currently under construction, check back soon :)
+      </p>
+      <small>site developed by ShivaMateria</small>
+    </div>
+  );
+}
+
+export default App;
