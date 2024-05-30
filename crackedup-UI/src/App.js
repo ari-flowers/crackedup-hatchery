30d5675c3ba54314285f8f2ed16eaabafa1b46e8
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2024-05-30 04:00:37 -0700

    App.js initial setup

--- crackedup-UI/src/App.js
+++ crackedup-UI/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import Dashboard from './components/Dashboard';
+// import Hatchery from './components/Hatchery';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to my hatchery!</h1>
-      <p>Currently under construction, check back soon :)
-      </p>
-      <small>site developed by ShivaMateria</small>
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Header /> */}
+        <h1>Welcome to Cracked Up Hatchery!</h1>
+        <p>Currently under construction, check back soon :)</p>
+        <small>site developed by ShivaMateria</small>
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/hatchery" element={<Hatchery />} />
+        </Routes> */}
+      </div>
+    </Router>
   );
 }
 
