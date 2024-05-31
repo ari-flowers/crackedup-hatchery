016e4184315502535a377f98fc446d4a4ef6e97b
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2024-05-31 16:28:31 -0700

    Update delete logic on the frontend, change required clicks to be number of entries if <10

--- crackedup-UI/src/components/DragonVillage/DragonVillage.js
+++ crackedup-UI/src/components/DragonVillage/DragonVillage.js
@@ -10,9 +10,10 @@ function DragonVillage() {
   const [selectedEgg, setSelectedEgg] = useState(null);
   const [clicks, setClicks] = useState(0);
   const [errorMessage, setErrorMessage] = useState('');
-  const REQUIRED_CLICKS = 1;
+  const [requiredClicks, setRequiredClicks] = useState(1);
 
   useEffect(() => {
+    //fetch all egg links
     fetch(DV_API_URL)
       .then(response => response.json())
       .then(data => {
@@ -22,19 +23,23 @@ function DragonVillage() {
           image: egg.image || '/images/default_image.png', // Handle null image
           submission_time: egg.submission_time || new Date().toISOString() // Handle null submission_time
         })));
+        //set required clicks to number of entries in the DB if <10
+        setRequiredClicks(data.length > 10 ? 10 : data.length)
       })
       .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
       });
   }, []);
 
+  //Egg link click handler
   const handleEggClick = (egg) => {
     setSelectedEgg(egg);
     setClicks(clicks + 1);
   };
 
+  //Handle egg link submission form
   const handleSubmit = (data) => {
-    if (clicks >= REQUIRED_CLICKS) {
+    if (clicks >= requiredClicks) {
       fetch(DV_API_URL, {
         method: 'POST',
         headers: {
@@ -57,28 +62,32 @@ function DragonVillage() {
           setClicks(0);
           setSelectedEgg(null);
           setErrorMessage('');
+          setRequiredClicks(eggs.length + 1 > 10 ? 10 : eggs.length + 1);
         })
         .catch(error => {
           console.error('Error submitting eggs:', error);
           setErrorMessage(error.error || 'An unknown error occurred.');
         });
     } else {
-      alert(`You need to click at least ${REQUIRED_CLICKS} eggs before adding your own!`);
+      alert(`You need to click at least ${requiredClicks} links before adding your own!`);
     }
   };
 
+  //Delete form handler
   const handleDelete = (shareLink) => {
-    fetch(`${DV_API_URL}/${encodeURIComponent(shareLink)}`, {
+    fetch(DV_API_URL, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
-      }
+      },
+      body: JSON.stringify({ share_link: shareLink })
     })
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         setEggs(eggs.filter(egg => egg.share_link !== shareLink));
+        setRequiredClicks(eggs.length - 1 > 10 ? 10 : eggs.length - 1);
       })
       .catch(error => {
         console.error('Error deleting egg:', error);
@@ -88,7 +97,9 @@ function DragonVillage() {
 
   return (
     <div className="dragon-village">
-      <SubmitEggForm onSubmit={handleSubmit} disabled={clicks < REQUIRED_CLICKS} />
+      {/* Submit Link form  */}
+      <SubmitEggForm onSubmit={handleSubmit} disabled={clicks < requiredClicks} />
+      {/* Delete via Link form */}
       <DeleteEggForm onSubmit={handleDelete} />
       {errorMessage && <div className="error-message">{errorMessage}</div>}
       <div className="egg-grid">
