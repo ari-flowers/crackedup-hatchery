b0c17a4375ca9f7f14a533393a939eae827e19ff
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2024-05-31 17:17:32 -0700

    Remove duplicate backend files from parent directory, remove crackedup-UI folder from crackedup-backend

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do
-    resources :dragon_village_eggs, only: [:index, :create]
-    delete 'dragon_village_eggs', to: 'dragon_village_eggs#destroy_by_link'
-  end
-end
