64bcfce25f9683d89e0d7d48e48c11fe2a0c9850
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2024-06-19 20:02:00 -0700

    Create routes for dummy dragon data to add and remove dragons

--- crackedup-backend/config/routes.rb
+++ crackedup-backend/config/routes.rb
@@ -1,17 +1,24 @@
 Rails.application.routes.draw do
   namespace :api do
+    # Dragon Village Collection link viewer
     resources :dragon_village_eggs, only: [:index, :create] do
       delete 'destroy_by_link', on: :collection
     end
+    # Dragon Cave hatchery
+    resources :dragons, only: [:index, :update, :destroy] do
+      collection do
+        post 'add_all'
+        delete 'remove_all'
+      end
+    end
   end
-
+  # Dragon Cave user routes
+  resources :users, only: [:show, :update] do
+    get 'dashboard', on: :member
+  end
+  
   # OmniAuth routes for Dragon Cave
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#failure'
   delete '/logout', to: 'sessions#destroy'
-  
-  # User routes
-  resources :users, only: [:show, :update] do
-    get 'dashboard', on: :member
-  end
 end
