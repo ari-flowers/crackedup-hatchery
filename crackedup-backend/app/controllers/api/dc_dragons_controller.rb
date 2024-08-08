5bc22fc7b87134e4425a1e298008ea60353b4dc3
Ari Flowers <87172647+ari-flowers@users.noreply.github.com>
2024-08-08 14:28:41 -0700

    Possibly bugged

--- crackedup-backend/app/controllers/api/dc_dragons_controller.rb
+++ crackedup-backend/app/controllers/api/dc_dragons_controller.rb
@@ -20,7 +20,44 @@ module Api
           unique_view_count: 100,
           clicks: 20,
           time_remaining: "2 days"
-        }
+        },
+        {
+          id: 3,
+          name: "Dragon 2",
+          image: "/images/default_cupifriend.png",
+          view_count: 200,
+          unique_view_count: 100,
+          clicks: 20,
+          time_remaining: "2 days"
+        },
+        {
+          id: 4,
+          name: "Dragon 2",
+          image: "/images/default_cupifriend.png",
+          view_count: 200,
+          unique_view_count: 100,
+          clicks: 20,
+          time_remaining: "2 days"
+        },
+        {
+          id: 5,
+          name: "Dragon 2",
+          image: "/images/default_cupifriend.png",
+          view_count: 200,
+          unique_view_count: 100,
+          clicks: 20,
+          time_remaining: "2 days"
+        },
+        {
+          id: 6,
+          name: "Dragon 2",
+          image: "/images/default_cupifriend.png",
+          view_count: 200,
+          unique_view_count: 100,
+          clicks: 20,
+          time_remaining: "2 days"
+        },
+
       ]
       render json: dummy_data
     end
